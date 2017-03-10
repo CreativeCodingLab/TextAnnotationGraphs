7d8c652f2311462a2d5e13c782fb63f09e4684ee
Hai Tran <hai.t@outlook.com>
2017-03-10 16:23:40 -0600

    First progress
    Words & Labels are on.
    Still unable to make split-right scrollable

--- js/step.js
+++
@@ -1,7 +0,0 @@
-// A part of the staircase. Each step is a square which contains either
-// a word or an excerpt of a sentence/paragraph.
-class Step {
-    constructor() {
-        //this.size
-    }
-}
