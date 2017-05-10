b4c23414f2eb1e90289fc760d4fd35a6380fe4fd
Kristine Lee <hmsklee@users.noreply.github.com>
2017-05-10 15:57:17 -0500

    modify parser.js to handle new json format

--- js/parser.js
+++ js/parser.js
@@ -1,183 +1,100 @@
-function readJson(path) {
-
-  var json = path || './data/angus-ex.json';
-
-  function loadJSON(path, callback) {
-      var httpRequest = new XMLHttpRequest();
-      httpRequest.onreadystatechange = function() {
-          if (httpRequest.readyState === 4) {
-              if (httpRequest.status === 200) {
-                  var data = JSON.parse(httpRequest.responseText);
-                  if (callback) callback(data);
-              }
-          }
-      };
-      httpRequest.open('GET', path);
-      httpRequest.send(); 
+class Parser {
+  constructor() {
+    this.text = "";
+    this.tokens = [];
+    this.data = {};
   }
 
-  /* parse json file*/
-  loadJSON(json, parseData);
-}
+  /* load a json from a path */
+  readJson(path, callback) {
 
+    var json = path || './data/data1.json';
 
-/** init words and links from json data
- *  - first hierarchy:  documents--sentences--words (+associated syntax tags)
- *  -                   edges connect parts of speech
- *  - second hierarchy: events--paths | arguments--themes
- */
-function parseData(data) {
-  let wordDataArray = [];
-  let wordDataMap = {};
-  let syntaxDataArray = [];
-  let mentionDataArray = [];
-
-  for (var i in data.documents) {
-    let doc = data.documents[i];
-
-    doc.sentences.forEach(function(sentence, j) {
-      // parse word data
-      let sentenceData = sentence.words.map(function(word, k) {
-
-        let wordDataObject = {
-          text: word,
-          documentId: i,
-          sentenceId: j,
-          locationInSentence: k,
-          charLocationInSentence: sentence.startOffsets[k],
-          syntaxData: {
-            tag: sentence.tags[k]
-          },
-          bioData: {
-            tag: ''
-          }
-        };
-
-        wordDataMap[[i,j,k].join('-')] = wordDataArray.length;
-        wordDataArray.push(wordDataObject);
-        return wordDataObject
-      });
-
-      // create POS links
-      sentence.graphs["stanford-collapsed"].edges.forEach(function(edge) {
-        syntaxDataArray.push({
-          destination: sentenceData[edge.destination],
-          label: edge.relation,
-          type: edge.relation,
-          source: sentenceData[edge.source]
-        })
-      });
-
-    })
-  }
-
-  // flatten data.mentions array
-  let printMention = function(mention, i) {
-    if (mention.arguments) {
-      for (var j in mention.arguments) {
-        mention.arguments[j] = mention.arguments[j].map(printMention)
-      }
-    }
-
-    switch (mention.type) {
-      case "CorefTextBoundMention":
-        // has text(s) only
-        let start = wordDataMap[[mention.document, mention.sentence, mention.tokenInterval.start].join('-')];
-        let end = wordDataMap[[mention.document, mention.sentence, mention.tokenInterval.end].join('-')];
-
-        var link = {
-          sourceId: null,
-          destinationId: null,
-          words: wordDataArray.slice(start, end),
-          label: mention.displayLabel,
-          id: mention.id,
-          charOffset: mention.characterStartOffset,
-          type: mention.type
-        };
-        mentionDataArray.push(link);
-        return link;
-      case "CorefRelationMention":
-        // has argument(s)
-        // hard-coded the property --- need better data to parse this correctly
-
-        let keys = Object.keys(mention.arguments);
-        if (keys.length != 2 || !mention.arguments.controlled || !mention.arguments.controller) {
-          console.log("bad data parse: check CorefRelationMention", mention.arguments);
-        }
-        var link = {
-          sourceId: mention.arguments.controller.map(arg => arg.id),
-          destinationId: [{
-            name: mention.displayLabel,
-            id: mention.arguments.controlled.map(arg => arg.id)
-          }],
-          label: mention.displayLabel,
-          id: mention.id,
-          charOffset: mention.characterStartOffset,
-          type: mention.type
-        };
-
-        mentionDataArray.push(link);
-        return link;
-      case "CorefEventMention":
-        // has a trigger & argument(s)
-
-        if (mention.trigger.type == "TextBoundMention") {
-          let start = wordDataMap[[mention.trigger.document, mention.trigger.sentence, mention.trigger.tokenInterval.start].join('-')];
-          let end = wordDataMap[[mention.trigger.document, mention.trigger.sentence, mention.trigger.tokenInterval.end].join('-')];
-
-          var link = {
-            sourceId: null,
-            destinationId: null,
-            words: wordDataArray.slice(start, end),
-            label: mention.displayLabel,
-            id: mention.trigger.id,
-            charOffset: mention.trigger.characterStartOffset,
-            type: mention.trigger.type
-          };
-          mentionDataArray.push(link);
-        }
-        var link = {
-          sourceId: [mention.trigger.id],
-          destinationId: Object.keys(mention.arguments).map(key => {
-
-            return {
-              name: key,
-              charOffset: mention.characterStartOffset,
-              id: mention.arguments[key].map(arg => arg.id)
+    function loadJSON(path, callback2) {
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function() {
+            if (httpRequest.readyState === 4) {
+                if (httpRequest.status === 200) {
+                    var data = JSON.parse(httpRequest.responseText);
+                    if (callback2) {
+                      callback2(data);
+                    }
+                    if (callback) {
+                      callback();
+                    }
+                }
             }
-
-          }),
-          label: mention.displayLabel,
-          id: mention.id,
-          type: mention.type
         };
-        mentionDataArray.push(link);
-        return link;
-      default:
-        console.log("invalid type", mention.type);
-        break;
+        httpRequest.open('GET', path);
+        httpRequest.send(); 
     }
+
+    /* parse json file*/
+    loadJSON(json, this.parseData.bind(this));
   }
-  data.mentions.forEach(printMention);
 
-  // done parsing into semi-flat datasets...
+  /**
+   * parse a json file
+   */
+  parseData(data) {
+    /* first get string tokens from the syntaxData */
+    this.text = data.syntaxData.text;
+    this.tokens = data.syntaxData.entities
+      .map(x => x[2][0])
+      .sort((a, b) => a[0] - b[0])
+      .map(interval => this.text.substring(interval[0], interval[1]));
 
-  console.log('words',wordDataArray);
-  console.log('pos',syntaxDataArray);
-  console.log('events',mentionDataArray);
-}
+    /* parse the event data: entities, triggers, events, and relations */
+    const e = data.eventData;
+    const entities = e.entities.map(arr => {
+          return {
+              id: arr[0],
+              type: arr[1],
+              interval: arr[2][0],
+              string: e.text.substring(arr[2][0][0], arr[2][0][1])
+          };
+      });
 
-document.querySelector('input[type=file]').onchange = function() {
-  var fr = new FileReader();
-  fr.onload = function(e) {
-    var object = {};
-    try {
-      object = JSON.parse(e.target.result);
-    }
-    catch(e) {
-      console.log("error",e);
-    }
-    parseData(object);
+    const triggers = e.triggers.map(arr => {
+          return {
+              id: arr[0],
+              type: arr[1],
+              interval: arr[2][0],
+              string: e.text.substring(arr[2][0][0], arr[2][0][1])
+          };
+      });
+
+    const events = e.events.map(arr => {
+          return {
+              id: arr[0],
+              trigger: arr[1],
+              arguments: arr[2].map(argument => {
+                  return {
+                      id: argument[1],
+                      type: argument[0]
+                  };
+              })
+          };
+      });
+
+    const relations = e.relations.map(arr => {
+          return {
+              id: arr[0],
+              type: arr[1],
+              arguments: arr[2].map(argument => {
+                  return {
+                      id: argument[1],
+                      type: argument[0]
+                  };
+              })
+          };
+      });
+
+    this.data = {
+      entities,
+      triggers,
+      events,
+      relations
+    };
   }
-  fr.readAsText(this.files[0]);
 }
